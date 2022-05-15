2bd2076468349bad2ac8af4cd715d75769613d04
Fathur Rahman <fathur.rahman@udana.id>
2022-05-16 00:15:46 +0700

    Memisahkan Header, Sidebar, dan Footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,31 +63,9 @@ AppAsset::register($this);
                     <br />
 
                     <!-- sidebar menu -->
-                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-
-                        <div class="menu_section">
-                            <h3>General</h3>
-                            <?=
-                            \yiister\gentelella\widgets\Menu::widget(
-                                [
-                                    "items" => [
-                                        ["label" => "Home", "url" => "site/index", "icon" => "home"],
-                                        ["label" => "Personal", "url" => ["personal/index"], "icon" => "fas fa-user"],
-                                        [
-                                            "label" => "Pegawai",
-                                            "icon" => "fas fa-users",
-                                            "url" => "#",
-                                            "items" => [
-                                                ["label" => "Data Pegawai", "url" => ["pegawai/index"]],
-                                            ],
-                                        ],
-                                    ],
-                                ]
-                            )
-                            ?>
-                        </div>
-
-                    </div>
+                    <?php $this->beginContent('@app/views/layouts/production/sidebar-menu.php'); ?>
+                    //...layout content here...
+                    <?php $this->endContent(); ?>
                     <!-- /sidebar menu -->
 
                     <!-- /menu footer buttons -->
@@ -110,67 +88,9 @@ AppAsset::register($this);
             </div>
 
             <!-- top navigation -->
-            <div class="top_nav">
-
-                <div class="nav_menu">
-                    <nav class="" role="navigation">
-                        <div class="nav toggle">
-                            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                        </div>
-
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="">
-                                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                    <img src="<?= Url::base(true) ?>/images/img.jpg" alt="">John Doe
-                                    <span class=" fa fa-angle-down"></span>
-                                </a>
-                                <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                    <li><a href="javascript:;"> Profile</a>
-                                    </li>
-                                    <li>
-                                        <?= Yii::$app->user->isGuest ? ('<a href="login.php?r=site/login"><i class="fa fa-sign-in pull-right"></i> Log In</a>'
-                                        ) : ('<a data-method="post" href="' . Url::to(['site/logout']) . '"><i class="fa fa-sign-out pull-right"></i> Log Out (' . Yii::$app->user->identity->username . ')</a>'
-                                        ) ?>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li role="presentation" class="dropdown">
-                                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-envelope-o"></i>
-                                    <span class="badge bg-green">1</span>
-                                </a>
-                                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                                    <li>
-                                        <a>
-                                            <span class="image">
-                                                <img src="<?= Url::base(true) ?>/images/img.jpg" alt="Profile Image" />
-                                            </span>
-                                            <span>
-                                                <span>John Smith</span>
-                                                <span class="time">3 mins ago</span>
-                                            </span>
-                                            <span class="message">
-                                                Film festivals used to be do-or-die moments for movie makers. They were where...
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <div class="text-center">
-                                            <a href="/">
-                                                <strong>See All Alerts</strong>
-                                                <i class="fa fa-angle-right"></i>
-                                            </a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-
-                        </ul>
-                    </nav>
-                </div>
-
-            </div>
+            <?php $this->beginContent('@app/views/layouts/production/header.php'); ?>
+            //...layout content here...
+            <?php $this->endContent(); ?>
             <!-- /top navigation -->
 
             <!-- page content -->
---
+++ views/layouts/production/sidebar-menu.php
@@ -0,0 +1,25 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
+  <div class="menu_section">
+    <h3>General</h3>
+    <?=
+    \yiister\gentelella\widgets\Menu::widget(
+      [
+        "items" => [
+          ["label" => "Home", "url" => "site/index", "icon" => "home"],
+          ["label" => "Personal", "url" => ["personal/index"], "icon" => "fas fa-user"],
+          [
+            "label" => "Pegawai",
+            "icon" => "fas fa-users",
+            "url" => "#",
+            "items" => [
+              ["label" => "Data Pegawai", "url" => ["pegawai/index"]],
+            ],
+          ],
+        ],
+      ]
+    )
+    ?>
+  </div>
+
+</div>
