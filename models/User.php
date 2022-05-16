ab4bd086c2dfca571545401caf35a1512dd4e1cc
Fathur Rahman <fathur.rahman@udana.id>
2022-05-16 12:23:05 +0700

    login

--- models/User.php
+++ models/User.php
@@ -2,30 +2,28 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
-{
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
+use Yii;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    const STATUS_ACTIVE = 10;
+    const STATUS_DELETE = 10;
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['time_create', 'time_update'], 'safe'],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETE]],
+        ];
+    }
 
 
     /**
@@ -33,7 +31,7 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
     }
 
     /**
@@ -41,7 +39,8 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
+        $users = self::find()->where(['accessToken' => $token])->all();
+        foreach ($users as $user) {
             if ($user['accessToken'] === $token) {
                 return new static($user);
             }
@@ -58,13 +57,7 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
 
     /**
@@ -101,4 +94,107 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     {
         return $this->password === $password;
     }
+
+    public function getPegawai()
+    {
+        return $this->hasOne(Pegawai::className(), ['id_pegawai' => 'id_pegawai']);
+    }
+
+    // public $id;
+    // public $username;
+    // public $password;
+    // public $authKey;
+    // public $accessToken;
+
+    // private static $users = [
+    //     '100' => [
+    //         'id' => '100',
+    //         'username' => 'admin',
+    //         'password' => 'admin',
+    //         'authKey' => 'test100key',
+    //         'accessToken' => '100-token',
+    //     ],
+    //     '101' => [
+    //         'id' => '101',
+    //         'username' => 'demo',
+    //         'password' => 'demo',
+    //         'authKey' => 'test101key',
+    //         'accessToken' => '101-token',
+    //     ],
+    // ];
+
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public static function findIdentity($id)
+    // {
+    //     return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+    // }
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public static function findIdentityByAccessToken($token, $type = null)
+    // {
+    //     foreach (self::$users as $user) {
+    //         if ($user['accessToken'] === $token) {
+    //             return new static($user);
+    //         }
+    //     }
+
+    //     return null;
+    // }
+
+    // /**
+    //  * Finds user by username
+    //  *
+    //  * @param string $username
+    //  * @return static|null
+    //  */
+    // public static function findByUsername($username)
+    // {
+    //     foreach (self::$users as $user) {
+    //         if (strcasecmp($user['username'], $username) === 0) {
+    //             return new static($user);
+    //         }
+    //     }
+
+    //     return null;
+    // }
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function getId()
+    // {
+    //     return $this->id;
+    // }
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function getAuthKey()
+    // {
+    //     return $this->authKey;
+    // }
+
+    // /**
+    //  * {@inheritdoc}
+    //  */
+    // public function validateAuthKey($authKey)
+    // {
+    //     return $this->authKey === $authKey;
+    // }
+
+    // /**
+    //  * Validates password
+    //  *
+    //  * @param string $password password to validate
+    //  * @return bool if password provided is valid for current user
+    //  */
+    // public function validatePassword($password)
+    // {
+    //     return $this->password === $password;
+    // }
 }
